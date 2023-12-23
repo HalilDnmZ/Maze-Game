48b10f596dbcb2159e7334946beaeb547de2a10a
HalilDnmZ <123744643+haldonmez@users.noreply.github.com>
2023-12-24 00:21:57 +0300

    Movement Update

---
+++ Assets/EchoEffect.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TextCore.Text;
+
+public class EchoEffect : MonoBehaviour
+{
+
+    public float timeBtwSpawns;
+    public float startTimeBtwSpawns;
+
+    public GameObject echo;
+    private CharacterMovement player;  
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<CharacterMovement>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.isDashing == true)
+        {
+            if(timeBtwSpawns <= 0)
+            {
+                GameObject instance = (GameObject)Instantiate(echo, transform.position, Quaternion.identity);
+                Destroy(instance, 8f);
+                timeBtwSpawns = startTimeBtwSpawns;
+            }
+            else
+            {
+                timeBtwSpawns -= Time.deltaTime;
+            }
+        }
+    }
+}
