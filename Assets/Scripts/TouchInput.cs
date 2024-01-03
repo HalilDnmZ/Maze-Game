9e82faf542553da0e54215847c2ec4780060af7a
HalilDnmZ <123744643+haldonmez@users.noreply.github.com>
2024-01-03 18:25:43 +0300

    Touch Response Changes

---
+++ Assets/Scripts/TouchInput.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchInput : MonoBehaviour
+{
+    public CharacterMovement player;
+    private Vector2 startPosition;
+    public int PixelDistanceToDetect = 20;
+    private bool fingerDown;
+
+    private void Update()
+    {
+        if (fingerDown == false && Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            startPosition = Input.touches[0].position;
+            fingerDown = true;
+        }
+
+        if (fingerDown)
+        {
+            if (Input.touches[0].position.y >= startPosition.y + PixelDistanceToDetect)
+            {
+                fingerDown = false;
+                Debug.Log("Swiped up");
+            }
+        }
+
+        //TESTING FOR PC
+
+        if(fingerDown == false && Input.GetMouseButtonDown(0))
+        {
+            startPosition = Input.mousePosition;
+            fingerDown = true;
+        }
+
+        if (fingerDown)
+        {
+            if(Input.mousePosition.y >= startPosition.y + PixelDistanceToDetect)
+            {
+                fingerDown = false;
+                Debug.Log("Swiped up on a Computer.");
+            }
+        }
+
+        if(fingerDown && Input.GetMouseButtonUp(0))
+        {
+            fingerDown = false;
+        }
+    }
+}
